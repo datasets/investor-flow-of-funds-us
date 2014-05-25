850cc70605afbc59ac1bedef561295b91e1b3c4b
Rufus Pollock <rufus.pollock@okfn.org>
2014-05-25 21:08:08 +0100

    [all][l]: complete set of long term mutual funds data done (with datapackage.json).

---
+++ README.md
@@ -0,0 +1,19 @@
+Monthly net new cash flow into various mutual fund investment classes
+(equities, bonds etc). Statistics come from the Investment Company Institute
+(ICI).
+
+## Data
+
+Data comes from the data provided on the [ICI Statistics pages][ici], in particular:
+
+* Summary: Estimated Long-Term Mutual Fund Flows Data (xls)
+
+[ici]: http://www.ici.org/research/stats
+
+Notes for Long-Term Mutual Fund Flows Data:
+
+* All figures are (nominal) millions of US dollars (USD)
+* Weekly cash flows are estimates based on reporting covering 98 percent of
+  industry assets, while monthly flows are actual numbers as reported in ICI's
+  "Trends in Mutual Fund Investing."
+
---
+++ scripts/process.py
@@ -0,0 +1,67 @@
+import csv
+import datetime
+import urllib
+
+from dataconverters import xls
+
+url = 'http://www.ici.org/info/flows_data_2014.xls'
+archive = 'archive/flows_data.xls'
+
+def download():
+    urllib.urlretrieve(url, archive)
+
+def extract():
+    fields = [ 
+        'Date',
+        'Total Equity',
+        'Domestic Equity',
+        'World Equity',
+        'Hybrid',
+        'Total Bond',
+        'Taxable Bond',
+        'Municipal Bond',
+        'Total'
+        ]
+    fin = open(archive)
+    records, metadata = xls.parse(fin)
+    records = [ [ r[f['id']] for f in metadata['fields'] ] for r in records ]
+    # discard first 2 rows as blank / table title
+    records = records[2:]
+    
+    for r in records:  
+        if isinstance(r[0], datetime.datetime):
+            # get rid of time part
+            r[0] = r[0].isoformat()[:10]
+        for idx in range(1, len(r)):
+            if isinstance(r[idx], float):
+                r[idx] = int(r[idx])
+
+    # split out weekly from monthly
+    # somewhere down sheet have blank line then heading for weekly stuff
+    for count, r in enumerate(records):
+        if r[0] == 'Estimated Weekly Net New Cash Flow':
+            weekly = records[count+1:count+8]
+            # -1 as blank line above
+            monthly = records[:count-1]
+
+    # here we just overwrite as they seem to append (earliest data still there)
+    fo = open('data/monthly.csv', 'w')
+    writer = csv.writer(fo)
+    writer.writerow(fields)
+    writer.writerows(monthly)
+
+    # TODO: we actually need to merge this with the existing data ...
+    fo = open('data/weekly.csv', 'w')
+    writer = csv.writer(fo)
+    writer.writerow(fields)
+    writer.writerows(weekly)
+
+
+def process():
+    download()
+    extract()
+
+if __name__ == '__main__':
+    download()
+    extract()
+
